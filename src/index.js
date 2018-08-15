da98a435eedecac526812d8260ad643d817af00e
Li Arolf Rey <liarolfrrey@gmail.com>
2018-08-15 21:56:50 +0800

    Add blockchain and node instance

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import uuid from 'uuid/v4';
 import setupRouter from './routes/routes';
+import Node from './models/Node';
+import Blockchain from './models/Blockchain';
 
 const app = express()
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }));
 
+const port = process.env.PORT || 3000;
+
+// instantiate the blockchain and node
+// pass the node instance to the routes
+
+const blockchain = new Blockchain();
+const node = new Node(uuid(), `http://localhost:${port}`, null, blockchain);
+
 // initializes routes for rest api
-setupRouter(app);
+setupRouter(app, node);
 
-app.get('/', (req, res) => {
-  res.set('Hello World');
-});
-
-app.listen(3000, () => console.log('Running at port 3000'));
+app.listen(port, () => console.log(`Running on port: ${port}`));
